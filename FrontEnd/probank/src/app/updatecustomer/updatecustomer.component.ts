1b8dbb99817d783f35cc517476626daa3ff82925
Yash <ldh.yashsharma2gmail.com>
2020-05-02 13:27:42 +0530

    Update Modules Front-End
    Date: 2nd May, 2020

---
+++ FrontEnd/probank/src/app/updatecustomer/updatecustomer.component.ts
@@ -0,0 +1,144 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UpdateserviceService } from '../updateservice.service';
+import { Customer } from '../customer';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-updatecustomer',
+  templateUrl: './updatecustomer.component.html',
+  styleUrls: ['./updatecustomer.component.css']
+})
+export class UpdatecustomerComponent implements OnInit {
+
+  constructor(private service:UpdateserviceService) { }
+
+  flag3:boolean = true;
+  @ViewChild("formdata")
+  form: NgForm;
+  cust:Customer[];
+  tempCust:Customer;
+  id:number;
+  ngOnInit(): void 
+  {
+    this.service.getAllcustomers().subscribe(data=>
+      {
+        this.cust = data;
+      },
+      error=>{
+        alert("error occured while subscribing data from server");
+        console.log("error occured while subscribing data from server");
+      });
+  }
+  update(id:number)
+  {
+    this.flag3 = false;
+    this.id = id;
+    this.service.getUgetcustomerByIDser(this.id).subscribe(data=>
+      {
+        this.tempCust = data;
+      },
+      error=>{
+        alert("error occured while fetching data from server for a perticular ID");
+        console.log("error occured while fetching data from server for a perticular ID");
+      });
+  }
+  cancel()
+  {
+    this.flag3 = true;
+  }
+  func()
+  {
+    this.service.updatec(this.id,this.tempCust).subscribe(data=>
+      {
+        alert("Updated Successfully");
+      },
+      error=>{
+        alert("error occured while sending data to the server for a perticular ID");
+        console.log("error occured while sending data to the server for a perticular ID");
+      });
+  }
+}
+
+// import { Component, ViewChild } from '@angular/core';
+// import { ClassStruct } from './class-struct';
+// import { ServiceComponentService } from './service-component.service';
+// import { NgForm } from '@angular/forms';
+// import { Router } from '@angular/router';
+
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent 
+// {
+//   temp:any="";
+//   title = 'probank';
+//   field:String;
+//   emp1:ClassStruct = new ClassStruct();
+//   prods:ClassStruct[];
+//   flag:boolean = false;
+//   flag1:boolean = true;
+//   flag3:boolean = true;
+//   constructor( private acter:ServiceComponentService, private route:Router ) {}
+//   @ViewChild("formdata")
+//   form: NgForm;
+//   ngOnInit():void 
+//   {
+//     this.acter.getProducts().subscribe((data) => this.prods = data);
+//   }
+//   selectedField(str:String)
+//   {
+//     this.field = str;
+//   }
+//   home()
+//   {
+//     this.acter.getProducts().subscribe((data) => this.prods = data);
+//     this.flag1=true;
+//     this.flag3=true;
+//   }
+//   add()
+//   {
+//     if(this.flag === false )
+//     {
+//       this.flag = true;
+//     }
+//     else
+//     {
+//       this.flag = false;
+//     }
+//   }
+//   submit()
+//   {
+//     this.prods.push(this.emp1);
+//     alert('Data added');
+//     this.emp1 = new ClassStruct();
+//     this.form.resetForm();
+//   }
+//   remove(prodId:number)
+//   {
+//     //this.prods = this.prods.filter( p => p.id != prodObj);
+//     this.prods.splice(prodId,1);
+//     alert("Data deleted");
+//   }
+//   update(prodId:number)
+//   {
+//     this.flag3 = false;
+//     this.prods = this.acter.updateReq(this.prods,prodId);
+//     if( this.flag1 === true )
+//     {
+//       this.flag1 = false;
+//     }
+//     this.acter.getProds();
+//   }
+//   cancel()
+//   {
+//     if( this.flag1 === false )
+//     {
+//       this.flag1 = true;
+//     }
+//     this.flag3 = true;
+//     this.flag=false;
+//   }
+// }
---
+++ FrontEnd/probank/src/app/updateservice.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Customer } from './customer';
+import { Address } from './address';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateserviceService 
+{
+  constructor(private _http:HttpClient) { }
+  
+  getAllcustomers():Observable<any>
+  {
+    let url = "http://localhost:3333/customers";
+    return this._http.get(url);
+  }
+  getUgetcustomerByIDser(id:number):Observable<any>
+  {
+    let url = "http://localhost:3333/customers/"+id;
+    return this._http.get(url);
+  }
+  updatec(id:number, customerUpdated:Customer):Observable<any>
+  {
+    let url = "http://localhost:3333/customers/"+id;
+    return this._http.post(url, customerUpdated, {responseType:'text'});
+  }
+  updateaddress(addressesId:number, addressUpdated:Address):Observable<any>
+  {
+    let url = "http://localhost:3333/customers/"+addressesId;
+    return this._http.post(url, addressUpdated, {responseType:'text'});
+  }
+}
